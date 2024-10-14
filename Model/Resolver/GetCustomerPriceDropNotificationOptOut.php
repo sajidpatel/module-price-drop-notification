0504ab18bbd6255d6856a711d82c68c80c971031
Sajid Patel <sajid.patel@interactivedezine.com>
2024-10-14 08:46:00 +0100

    Fixed GetCustomerPriceDropNotificationOptOut resolver

--- Model/Resolver/GetCustomerPriceDropNotificationOptOut.php
+++ Model/Resolver/GetCustomerPriceDropNotificationOptOut.php
@@ -8,20 +8,47 @@ use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\Exception\LocalizedException;
+use Psr\Log\LoggerInterface;
 
+/**
+ * Resolver for getting customer's price drop notification opt-out status
+ */
 class GetCustomerPriceDropNotificationOptOut implements ResolverInterface
 {
+    /**
+     * @var CustomerRepositoryInterface
+     */
     private $customerRepository;
+
+    /**
+     * @var CustomerSession
+     */
     private $customerSession;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param CustomerSession $customerSession
+     * @param LoggerInterface $logger
+     */
     public function __construct(
         CustomerRepositoryInterface $customerRepository,
-        CustomerSession $customerSession
+        CustomerSession $customerSession,
+        LoggerInterface $logger
     ) {
         $this->customerRepository = $customerRepository;
         $this->customerSession = $customerSession;
+        $this->logger = $logger;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function resolve(
         Field $field,
         $context,
@@ -29,27 +56,43 @@ class GetCustomerPriceDropNotificationOptOut implements ResolverInterface
         array $value = null,
         array $args = null
     ) {
-        // Case 1: Resolving as part of Customer type
-        if (isset($value['model'])) {
-            /** @var \Magento\Customer\Model\Data\Customer $customer */
-            $customer = $value['model'];
-            $attribute = $customer->getCustomAttribute('price_drop_notification_opt_out');
-            return $attribute ? (bool)$attribute->getValue() : false;
-        }
+        $result = null;
 
-        // Case 2: Resolving as standalone query
-        if (!$this->customerSession->isLoggedIn()) {
-            return null; // Return null if no customer is logged in
-        }
-
-        $customerId = $this->customerSession->getCustomerId();
         try {
-            $customer = $this->customerRepository->getById($customerId);
-            $attribute = $customer->getCustomAttribute('price_drop_notification_opt_out');
-            return $attribute ? (bool)$attribute->getValue() : false;
+            if ($this->customerSession->isLoggedIn()) {
+                $result = $this->resolveFromLoggedInCustomer();
+            }
         } catch (\Exception $e) {
-            // Log the exception if needed
-            return null;
+            $this->logger->error('Error resolving price drop notification opt-out status: ' . $e->getMessage(), [
+                'exception' => $e,
+            ]);
         }
+
+        return $result;
+    }
+
+    /**
+     * Resolve opt-out status from customer model
+     *
+     * @param \Magento\Customer\Model\Data\Customer $customer
+     * @return bool
+     */
+    private function resolveFromCustomerModel($customer): bool
+    {
+        $attribute = $customer->getCustomAttribute('price_drop_notification_opt_out');
+        return $attribute ? (bool)$attribute->getValue() : false;
+    }
+
+    /**
+     * Resolve opt-out status for logged-in customer
+     *
+     * @return bool
+     * @throws LocalizedException
+     */
+    private function resolveFromLoggedInCustomer(): bool
+    {
+        $customerId = $this->customerSession->getCustomerId();
+        $customer = $this->customerRepository->getById($customerId);
+        return $this->resolveFromCustomerModel($customer);
     }
 }
